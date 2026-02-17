18cc88cf8c52209bae46466a12879779ddb83976
XfheDev <cinarserif55akin@gmail.com>
2026-02-17 19:11:34 +0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
     schema: "prisma/schema.prisma",
     datasource: {
-        // This provides the URL for migration/push/generate steps
-        url: process.env.DATABASE_URL || "file:./dev.db"
+        url: process.env.TURSO_DATABASE_URL || process.env.DATABASE_URL || "file:./dev.db"
     }
 });
